d2d088c62cc06fb272e95a9d6cc40458a5300d70
Jesus Ordosgoitty <jesuodz@gmail.com>
2020-07-05 15:22:16 -0400

    Add models and database

---
+++ app/Http/Controllers/WithholdingController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Withholding;
+use Illuminate\Http\Request;
+
+class WithholdingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Withholding  $withholding
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Withholding $withholding)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Withholding  $withholding
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Withholding $withholding)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Withholding  $withholding
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Withholding $withholding)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Withholding  $withholding
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Withholding $withholding)
+    {
+        //
+    }
+}
---
+++ app/Withholding.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable as Auditable;
+use OwenIt\Auditing\Auditable as Audit;
+
+class Withholding extends Model implements Auditable 
+{
+    use SoftDeletes;
+    use Audit;
+
+    protected $table = 'withholdings';
+
+    protected $fillable = [
+        'user_id',
+        'affidavit_id',
+        'amount'
+    ];
+
+    public function affidavit()
+    {
+        return $this->belongsTo(Affidavit::class);
+    }
+
+    public function taxpayer()
+    {
+        return $this->hasOneThrough(Taxpayer::class, Affidavit::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
