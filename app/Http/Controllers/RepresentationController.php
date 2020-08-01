afc298c2a208195fa309725a7a6daeafc7508f85
Jesus Ordosgoitty <jesuodz@gmail.com>
2020-08-01 14:16:11 -0400

    Do not require commercial denomination

--- app/Http/Controllers/RepresentationController.php
+++ app/Http/Controllers/RepresentationController.php
@@ -28,10 +28,9 @@ class RepresentationController extends Controller
      */
     public function create(Taxpayer $taxpayer)
     {
-        if (($taxpayer->taxpayerType->description != 'JURÍDICO') && 
-            (!$taxpayer->commercialDenomination->exists())) {
-                return redirect('taxpayers/'.$taxpayer->id)
-                    ->withError('¡Este contribuyente no admite un representante!');
+        if ($taxpayer->taxpayerType->description != 'JURÍDICO') {
+            return redirect('taxpayers/'.$taxpayer->id)
+                ->withError('¡Este contribuyente no admite un representante!');
         }
 
         return view('modules.taxpayers.representations.register')
