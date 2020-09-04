86738e97a1dab1091a496d45cd4155a560c77c01
Jesus Ordosgoitty <jesuodz@gmail.com>
2020-09-04 06:48:14 -0400

    Add cdn links for testing

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,11 +7,11 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>SAUCO</title>
-    @include('layouts.styles')
-    <script src="{{ asset('css/app.css') }}"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
 </head>
 <body>
-    <div id="root"></div>
-    <script src="{{ asset('js/app.js') }}"></script>
+    <div id="app"></div>
+    <script src="https://unpkg.com/@material-ui/core@latest/umd/material-ui.production.min.js"></script>
 </body>
 </html>
