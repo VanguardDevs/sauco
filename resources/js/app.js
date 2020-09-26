a8c5e83dcd36e906556355ce5557dd7b9abc9ba3
Jesus Ordosgoitty <jesuodz@gmail.com>
2020-09-26 08:41:33 -0400

    Add packages

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,8 @@
  * building robust, powerful web applications using React + Laravel.
  */
 require('./bootstrap');
+const helloWorld = require('./hello-world').helloWorld();
+console.log(helloWorld);
 
 /**
  * Next, we will create a fresh React component instance and attach it to
@@ -11,5 +13,5 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-require('./index');
+// require('./index');
 
