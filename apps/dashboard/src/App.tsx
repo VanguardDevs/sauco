88d401562d43fb33f551f5c9b65e782926a653b0
Jesus Ordosgoitty <jesuodz@gmail.com>
2021-01-05 13:45:59 -0400

    Fix bug: module not declared 'ra-laravel-client'

--- apps/dashboard/src/App.tsx
+++ apps/dashboard/src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Admin, Resource } from 'react-admin';
+import apiClient from 'ra-laravel-client';
+import { history } from './utils';
+import { Dashboard } from './dashboard';
+import users from './users';
+import customRoutes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Admin
+      dashboard={Dashboard}
+      history={history}
+      customRoutes={customRoutes}
+      dataProvider={apiClient('http://dev.sauco.loc/api')}
+    > 
+      <Resource name='users' {...users} />
+    </Admin>
   );
 }
 
