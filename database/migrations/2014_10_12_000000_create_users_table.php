17d1320a0ae2ed4b64b76af5760130079af9efdb
Jesus Ordosgoitty <jesuodz@gmail.com>
2021-01-08 22:43:53 -0400

    Login method

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -25,6 +25,7 @@ class CreateUsersTable extends Migration
             $table->boolean('active')->default(1);
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
