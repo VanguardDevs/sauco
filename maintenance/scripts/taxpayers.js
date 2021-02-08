8f6abb8206b16c02083cde6d81384464d79a15cc
Jesus Ordosgoitty <jesuodz@gmail.com>
2021-02-07 23:21:04 -0400

    Migrate companies

--- maintenance/scripts/taxpayers.js
+++ maintenance/scripts/taxpayers.js
@@ -6,6 +6,28 @@ async function taxpayers() {
   try {
     // Rename table companies
     await db.schema.renameTable('commercial_denominations', 'companies');
+
+    await db.schema.table('companies', (table) => {
+      table.string('address');
+      table.decimal('capital', 2);
+      table.integer('num_workers');
+    });
+
+    const taxpayers = await db.select(
+        'taxpayers.name as name', 
+        'taxpayers.fiscal_address as address',
+        'taxpayers.id as taxpayer_id',
+        'taxpayers.created_at as created_at',
+        'taxpayers.updated_at as updated_at',
+        'taxpayers.deleted_at as deleted_at'
+      ).from('taxpayers')
+      .leftJoin('companies', 'taxpayers.id', 'companies.taxpayer_id')
+      .whereNull('companies.taxpayer_id')
+      .then(async rows => {
+        await db('companies').insert(rows);
+
+        console.log(`Created ${rows.length} companies`)
+      });
   } finally {
     await db.destroy();
   }
