82e328d2e32829dd91021aaec3d450e055c64124
Jesus Ordosgoitty <jesuodz@gmail.com>
2021-04-09 14:20:06 -0400

    View of listing movements

--- core/dashboard/src/App.tsx
+++ core/dashboard/src/App.tsx
@@ -5,6 +5,7 @@ import { history } from './utils';
 import { Dashboard } from './dashboard';
 import users from './users';
 import concepts from './concepts';
+import movements from './movements';
 import liquidations from './liquidations';
 import customRoutes from './routes';
 
@@ -19,6 +20,7 @@ function App() {
       <Resource name='users' {...users} />
       <Resource {...liquidations} />
       <Resource {...concepts} />
+      <Resource {...movements} />
     </Admin>
   );
 }
