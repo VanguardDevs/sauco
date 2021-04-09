668adf1ec2cd98140dd67a4283698a10ec7dfe93
Jesus Ordosgoitty <jesuodz@gmail.com>
2021-04-09 15:55:14 -0400

    Fix cors

--- core/backend/app/Http/Kernel.php
+++ core/backend/app/Http/Kernel.php
@@ -42,7 +42,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             EnsureFrontendRequestsAreStateful::class,
-            'throttle:api',
+            'throttle:60,1,api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
