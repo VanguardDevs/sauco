5d228212e364c62a6ba126d8224662fc7343732b
Jesus Ordosgoitty <jesuodz@gmail.com>
2021-05-13 10:32:28 -0400

    Update theme

---
+++ core/dashboard/src/i18nProvider.ts
@@ -0,0 +1,19 @@
+import spanishMessages from '@blackbox-vision/ra-language-spanish';
+import polyglotI18nProvider from 'ra-i18n-polyglot';
+
+const i18nProvider = polyglotI18nProvider(() => ({
+  ...spanishMessages,
+  page: {
+    dashboard: "Inicio",
+  },
+  pos: {
+    menu: {
+      'reports': 'Reportes',
+      'taxpayers': 'Contribuyentes',
+      'settings': 'Configuraciones',
+      'institution': 'Administración'
+    }
+  }
+}));
+
+export default i18nProvider;
