e0bcf36fd1b60286c2263ef3b009815031d584b1
Jesus Ordosgoitty <jesuodz@gmail.com>
2021-05-29 20:29:40 -0400

    Multiple changes in controllers

--- core/backend/app/Http/Controllers/VehicleModelController.php
+++ core/backend/app/Http/Controllers/VehicleModelController.php
@@ -18,16 +18,6 @@ class VehicleModelController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -45,17 +35,6 @@ class VehicleModelController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(VehicleModel $vehicleModel)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\VehicleModel  $vehicleModel
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(VehicleModel $vehicleModel)
     {
         //
     }
