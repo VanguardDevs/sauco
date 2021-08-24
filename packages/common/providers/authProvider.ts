4b0e9b882e035e70e055abbde28255d9503ec937
Jesus Ordosgoitty <jesuodz@gmail.com>
2021-08-24 12:25:32 -0400

    Fix authentication provider

--- packages/common/providers/authProvider.ts
+++ packages/common/providers/authProvider.ts
@@ -1,59 +1,65 @@
 import { AuthProvider } from 'react-admin';
 import apiClient from './dataProvider';
 
+const CONFIG_NAMES = {
+  IDENTIFICATION: `${process.env.REACT_APP_IDENTIFICATIONS_NAME}`,
+  AUTH_TOKEN: `${process.env.REACT_APP_AUTH_TOKEN_NAME}`,
+  PERMISSIONS: `${process.env.REACT_APP_PERMISSIONS_NAME}`,
+}
+
 const authProvider: AuthProvider = {
   login: async (data) => {
     await apiClient.get('csrf-cookie');
-    const response = await apiClient.post('tokens/login', data);
+    const response = await apiClient.post('login', data);
 
     if (response.status < 200 || response.status >= 300) {
       throw new Error(response.statusText);
     } else {
       if (response.token) {
-        await localStorage.setItem('saucoperms', response.permissions);
-        await localStorage.setItem('saucoauth', response.token);
-        await localStorage.setItem('saucoiden', response.user);
+        await localStorage.setItem(CONFIG_NAMES.IDENTIFICATION, response.user);
+        await localStorage.setItem(CONFIG_NAMES.AUTH_TOKEN, response.token);
+        await localStorage.setItem(CONFIG_NAMES.PERMISSIONS, response.permissions);
       }
 
-      return Promise.resolve();
+      return Promise.resolve(response.data);
     }
   },
   logout: async () => {
-    const response = await apiClient.post('logout');
+    await localStorage.removeItem(CONFIG_NAMES.AUTH_TOKEN);
+    await localStorage.removeItem(CONFIG_NAMES.IDENTIFICATION);
+    await localStorage.removeItem(CONFIG_NAMES.PERMISSIONS);
 
-    if (response.status < 200 || response.status >= 300) {
-      throw new Error(response.statusText);
-    } else {
-      await localStorage.removeItem('saucoauth');
-      await localStorage.removeItem('saucoperms');
-      await localStorage.removeItem('saucoiden');
-
-      return Promise.resolve();
-    }
+    return Promise.resolve();
   },
-  checkError: (error) => {
+  checkError: async (error) => {
     const { response } = error;
 
     if (response.status === 401 || response.status === 403) {
-      localStorage.removeItem('saucoauth');
+      await localStorage.removeItem(CONFIG_NAMES.AUTH_TOKEN);
+      await localStorage.removeItem(CONFIG_NAMES.IDENTIFICATION);
+      await localStorage.removeItem(CONFIG_NAMES.PERMISSIONS);
       return Promise.reject({ message: false });
     }
 
     return Promise.resolve();
   },
-  checkAuth: async () => await localStorage.getItem('saucoauth')
+  checkAuth: async () => await localStorage.getItem(CONFIG_NAMES.AUTH_TOKEN)
     ? Promise.resolve()
     : Promise.reject({ message: false }),
-  getPermissions: () => Promise.reject('Unknown method'),
+  getPermissions: async () => {
+    const permissions = await localStorage.getItem(CONFIG_NAMES.PERMISSIONS);
+
+    return permissions ? Promise.resolve(permissions) : Promise.resolve('guest');
+  },
   getIdentity: async () => {
-    const token = await localStorage.getItem('saucoiden');
+    const token = await localStorage.getItem(CONFIG_NAMES.IDENTIFICATION);
 
     if (token) {
       const { id, full_name, picture, ...rest } = JSON.parse(token);
 
       return ({
         id: id,
-        fullName: full_name,
+        full_name: full_name,
         avatar: picture,
         ...rest
       });
