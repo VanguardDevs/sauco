c517ae68ba3db72517a9c585f84e937b41b058b1
Jesus Ordosgoitty <jesuodz@gmail.com>
2021-10-11 14:48:37 -0400

    Create users form

---
+++ packages/dashboard/src/users/UsersList.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import {
+    TextInput,
+    List,
+    Datagrid,
+    TextField,
+    SimpleList,
+    TopToolbar,
+    CreateButton,
+    BooleanInput
+} from 'react-admin';
+import { Theme, useMediaQuery } from '@material-ui/core';
+
+const usersFilters = [
+    <TextInput label="login" source='Login' />,
+    <BooleanInput label='Activo' source='status' />
+];
+
+const ListActions: React.FC = props => (
+    <TopToolbar>
+        <CreateButton basePath="/users" />
+    </TopToolbar>
+);
+
+const UsersList: React.FC = props => {
+  const isSmall = useMediaQuery<Theme>(theme => theme.breakpoints.down('sm'));
+
+    return (
+        <List
+            title="Usuarios"
+            filters={usersFilters}
+            actions={<ListActions />}
+            {...props}
+        >
+            {
+                isSmall
+                ? (
+                    <SimpleList
+                        primaryText={record => `${record.login}`}
+                        linkType={"show"}
+                    />
+                )
+                : (
+                    <Datagrid>
+                        <TextField source="login" label="Login"/>
+                    </Datagrid>
+                )
+            }
+        </List>
+    );
+};
+
+export default UsersList;
