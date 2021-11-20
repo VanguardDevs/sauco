406f72c44fe1a7a2f65a579d4f02877464ccef89
Jesus Ordosgoitty <jesuodz@gmail.com>
2021-11-20 17:32:36 -0400

    Download invoices

---
+++ packages/backend/resources/views/pdf/bills/layouts/header.blade.php
@@ -0,0 +1,18 @@
+<div class="header">
+    <div class="sumatLOGO">
+        <img src="{{ asset('images/sumat.png') }}" height="90px" width="220px" alt="sumatlogo"/>
+    </div>
+    <div class="description">
+        <span>
+            REPÚBLICA BOLIVARIANA DE VENEZUELA<br>
+            ESTADO SUCRE<br>
+            ALCALDÍA DEL MUNICIPIO BERMÚDEZ<br>
+            SUPERINTENDENCIA MUNICIPAL DE ADMINISTRACIÓN TRIBUTARIA<br>
+            RIF: G-20000222-1<br>
+            DIRECCIÓN: AV. CARABOBO, EDIFICIO MUNICIPAL</br>
+        </span>
+    </div>
+    <div id="mayorLOGO">
+        <img src="{{ asset('images/escudo.jpg') }}" height="80px" width="100px" alt="logo" />
+    </div>
+</div>
