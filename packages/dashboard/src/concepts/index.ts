aa33e3c5bc73c803297a4a463c00bf5c27eea093
Jesus Ordosgoitty <jesuodz@gmail.com>
2021-12-28 11:46:59 -0400

    Edit concepts

--- packages/dashboard/src/concepts/index.ts
+++ packages/dashboard/src/concepts/index.ts
@@ -1,11 +1,13 @@
 import LocalGroceryStoreIcon from '@material-ui/icons/LocalGroceryStore'
 import ConceptsList from './ConceptList'
 import ConceptCreate from './ConceptCreate'
+import ConceptEdit from './ConceptEdit'
 
 export default {
   name: 'concepts',
   icon: LocalGroceryStoreIcon,
   list: ConceptsList,
+  edit: ConceptEdit,
   create: ConceptCreate,
   options: {
     label: 'Conceptos'
