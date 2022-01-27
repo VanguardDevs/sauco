a641fb6be94c323e72b04e3364571fcc439bc017
Jesus Ordosgoitty <jesuodz@gmail.com>
2022-01-27 11:52:38 -0400

    Format license

--- packages/sirim/resources/views/modules/licenses/pdf/economic-activity-license.blade.php
+++ packages/sirim/resources/views/modules/licenses/pdf/economic-activity-license.blade.php
@@ -39,24 +39,24 @@
             .container {
                 position: relative;
                 display: block;
-                top: 180px;
+                top: 200px;
                 left: 14px;
                 overflow: hidden;
                 width: 670px;
-                height: 850px;
+                height: 785px;
                 z-index: 9;
             }
             .c-text-right {
                 width: 40%;
                 text-align: right;
-                font-size: 16px;
+                font-size: 15px;
                 font-weight: 700;
                 float: left;
             }
             .c-text-left {
                 width: 60%;
                 text-align: left;
-                font-size: 16px;
+                font-size: 15px;
                 font-weight: 400;
                 float: right;
             }
@@ -65,22 +65,27 @@
                 width: 100%;
                 height: 200px;
             }
-                .qr-code {
-                    width: 50%;
+                .qr-code-container {
+                    width: 100%;
                     height: 150px;
                     float: left;
-                    text-align: center;
-                    padding-top: 30px;
+                    padding-left: 21%;
+                    padding-top: 40px;
                 }
+                    .qr-code {
+                        width: 122px;
+                        height: 122px;
+                    }
                 .header-title {
-                    margin-top: 10px;
-                    height: 150px;
-                    width: 50%;
+                    margin-top: 34px;
+                    height: 130px;
+                    width: 40%;
                     float: right;
                     font-size: 28px;
                     font-weight: 700;
                     color: red;
-                    line-height: 35px;
+                    line-height: 26px;
+                    margin-right: 120px;
                 }
                 .license-num {
                     color: black
@@ -133,8 +138,14 @@
         </div>
         <div class="container">
             <div class="header">
-                <div class="qr-code">
-                    <img src="data:image/png;base64, {{ base64_encode(QrCode::size(150)->generate($qrLicenseString)) }} ">
+                <div class="qr-code-container">
+                    <div class="qr-code">
+                        <img 
+                            src="data:image/png;base64, {{ base64_encode(QrCode::size(150)->generate($qrLicenseString)) }}"
+                            width="100%"
+                            height="100%"
+                        />
+                    </div>
                 </div>
                 <div class="header-title">
                     LICENCIA DE ACTIVIDADES ECONÓMICAS <br/>Nº
@@ -207,7 +218,6 @@
                 </div>
             </div>
             <div class="bottom text-center">
-                <span class="row"><hr/></span>
                 <span class="row">{{ $signature->title }}</span>
                 <span class="row">superintendente de administración tributaria</span>
                 <span class="row">{{ $signature->decree }}</span>
