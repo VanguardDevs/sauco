38cb9c40327b252c398d8397ce440baf65872ac4
Jesus Ordosgoitty <jesuodz@gmail.com>
2022-02-07 15:24:55 -0400

    Fix bug in rates: economic activity

--- packages/sirim/app/Providers/AppServiceProvider.php
+++ packages/sirim/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\Builder;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +26,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        Builder::macro('whereLike', function (string $attribute, string $searchTerm) {
+            return $this->where($attribute, 'ILIKE', "%{$searchTerm}%");
+        });
     }
 }
