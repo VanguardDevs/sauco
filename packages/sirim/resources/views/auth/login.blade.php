8f029b86e3279cd27598123c454f72877d05e1f5
Jesus Ordosgoitty <jesuodz@gmail.com>
2022-02-24 23:58:41 -0400

    Delete logo from login

--- packages/sirim/resources/views/auth/login.blade.php
+++ packages/sirim/resources/views/auth/login.blade.php
@@ -8,11 +8,6 @@
     <div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--desktop kt-grid--ver-desktop kt-grid--hor-tablet-and-mobile">
         <div class="kt-grid__item kt-grid__item--fluid kt-grid__item--order--table-and-mobile-1 kt-login__wrapper">
             <div class="kt-login__body" style="justify-content:space-evenly">
-                <div class="kt-login__logo">
-                    <a href="">
-                        <img class="img-responsive" src="{{ asset('assets/images/logo1.png') }}">
-                    </a>
-                </div>
                 <div class="kt-login__form">
                     <div class="kt-login__title">
                        <h3>Inicio de Sesión</h3>
