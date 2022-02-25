4a19be11b99d6df6a2b44213862ce93423cf66a4
Jesus Ordosgoitty <jesuodz@gmail.com>
2022-02-25 19:51:54 -0400

    Update dataprovider

--- packages/common/providers/dataProvider.ts
+++ packages/common/providers/dataProvider.ts
@@ -1,6 +1,6 @@
 import apiClient from '@jodaz_/data-provider';
 
-const providers = apiClient(`${process.env.REACT_APP_API_DOMAIN}/api`, {
+const providers = apiClient(`${process.env.REACT_APP_API_DOMAIN}`, {
     withCredentials: true,
     offsetPageNum: 0
 }, `${process.env.REACT_APP_AUTH_TOKEN_NAME}`);
