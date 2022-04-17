761e7a70c6cf0895c66f24f2796e772cb0bfb26a
Luisandra29 <luisandrasifontes@gmail.com>
2022-04-17 13:24:14 -0400

    Update TestSeeder

--- packages/backend/database/seeders/TestSeeder.php
+++ packages/backend/database/seeders/TestSeeder.php
@@ -14,6 +14,7 @@ use App\Models\License;
 use App\Models\LiqueurClassification;
 use App\Models\LiqueurZone;
 use App\Models\LiqueurParameter;
+use App\Models\Liqueur;
 
 
 use Illuminate\Database\Seeder;
@@ -28,118 +29,120 @@ class TestSeeder extends Seeder
     public function run()
     {
 
-        // Taxpayer::create([
-        //     'rif' => '12345',
-        //     'name' => 'alguien',
-        //     'address' => 'direccion',
-        //     'phone' => '123456',
-        //     'email' => 'example@gmail.com',
-        //     'taxpayer_type_id' => '2',
-        //     'parish_id' => '1',
-        //     'community_id' => '1'
-        //     'taxpayer_classification_id' => '1'
-        // ]);
+        Taxpayer::create([
+            'id' => '1',
+             'rif' => '12345',
+             'name' => 'alguien',
+             'address' => 'direccion',
+             'phone' => '123456',
+             'email' => 'example@gmail.com',
+             'taxpayer_type_id' => '2',
+             'parish_id' => '1',
+             'community_id' => '1',
+             'taxpayer_classification_id' => '1'
+         ]);
 
 
-        // Liquidation::create([
-        //     'num' => '10123',
-        //     'amount' => '12',
-        //     'liquidable_id' => '1',
-        //     'liquidable_type' => 'algo',
-        //     'liquidation_type_id' => '1',
-        //     'taxpayer_id' => '1',
-        //     'status_id' => '1',
-        //     'concept_id' => '1',
-        //     'user_id' => '1'
-        // ]);
+         Liquidation::create([
+             'num' => '10123',
+             'amount' => '12',
+             'liquidable_id' => '1',
+             'liquidable_type' => 'algo',
+             'liquidation_type_id' => '1',
+             'taxpayer_id' => '1',
+             'status_id' => '1',
+             'concept_id' => '1',
+             'user_id' => '1'
+         ]);
 
 
-        // CorrelativeType::create([
-        //     'description' => 'algo'
+         CorrelativeType::create([
+             'description' => 'algo'
 
-        // ]);
+         ]);
 
-        // CorrelativeNumber::create([
-        //     'num' => '12345'
+         CorrelativeNumber::create([
+             'num' => '12345'
 
-        // ]);
+         ]);
 
-        // Year::create([
-        //     'year' => '2022'
+         Year::create([
+             'year' => '2022'
 
-        // ]);
+         ]);
 
-        // Correlative::create([
-        //     'correlative_type_id' => '1',
-        //     'year_id' => '1',
-        //     'correlative_number_id' => '1'
+         Correlative::create([
+             'correlative_type_id' => '1',
+             'year_id' => '1',
+             'correlative_number_id' => '1'
 
-        // ]);
+         ]);
 
-        // Company::create([
-        //     'name' => 'algun nombre',
-        //     'num_workers' => '5',
-        //     'address' => 'direccion',
-        //     'capital' => '12',
-        //     'constitution_date' => '22-03-2022',
-        //     'register_num' => '00000',
-        //     'register_volume' => '11111',
-        //     'register_casefile' => '01010',
-        //     'parish_id' => '1',
-        //     'community_id' => '1',
-        //     'taxpayer_id' => '1',
-        //     'phone' => '12365458',
-        //     'email' => 'company@correo.com'
-        // ]);
+         Company::create([
+             'name' => 'algun nombre',
+             'num_workers' => '5',
+             'address' => 'direccion',
+             'capital' => '12',
+             'constitution_date' => '22-03-2022',
+             'register_num' => '00000',
+             'register_volume' => '11111',
+             'register_casefile' => '01010',
+             'parish_id' => '1',
+             'community_id' => '1',
+             'taxpayer_id' => '1',
+             'phone' => '12365458',
+             'email' => 'company@correo.com'
+         ]);
 
-        // Representation::create([
-        //     'taxpayer_id' => '3',
-        //     'company_id' => '3',
-        //     'representation_type_id' => '1'
+         Representation::create([
+            'id' => '1',
+             'taxpayer_id' => '1',
+             'company_id' => '1',
+             'representation_type_id' => '1'
 
-        // ]);
+         ]);
 
-        // License::create([
-        //     'num' => '10123',
-        //     'active' => '1',
-        //     'emission_date' => '01-01-2020',
-        //     'expiration_date' => '01-01-2023',
-        //     'taxpayer_id' => '3',
-        //     'user_id' => '1',
-        //     'representation_id' => '2',
-        //     'correlative_id' => '1',
-        //     'ordinance_id' => '1',
+         License::create([
+             'num' => '10123',
+             'active' => '1',
+             'emission_date' => '01-01-2020',
+             'expiration_date' => '01-01-2023',
+             'taxpayer_id' => '1',
+             'user_id' => '1',
+             'representation_id' => '1',
+             'correlative_id' => '1',
+             'ordinance_id' => '1'
 
-        // ]);
-
-
-        // LiqueurClassification::create([
-        //     'name' => 'algo',
-        //     'abbreviature' => 'AL'
-
-        // ]);
-
-        // LiqueurZone::create([
-        //     'name' => 'algo'
-        // ]);
-
-        // LiqueurParameter::create([
-        //     'liqueur_classification_id' => '1',
-        //     'liqueur_zone_id' => '1',
-        //     'new_registry_amount' => '10',
-        //     'renew_registry_amount' => '11',
-        //     'movil' => '1'
-        // ]);
+         ]);
 
 
-        // Liqueur::create([
-        //     'work_hours' => '2',
-        //     'company_id' => '3',
-        //     'liqueur_parameter_id' => '1',
-        //     'representation_id' => '2',
-        //     'license_id' => '1'
+         LiqueurClassification::create([
+             'name' => 'algo',
+             'abbreviature' => 'AL'
 
-        // ]);
+         ]);
+
+         LiqueurZone::create([
+             'name' => 'algo'
+         ]);
+
+         LiqueurParameter::create([
+             'liqueur_classification_id' => '1',
+             'liqueur_zone_id' => '1',
+             'new_registry_amount' => '10',
+             'renew_registry_amount' => '11',
+             'movil' => '1'
+         ]);
+
+
+         Liqueur::create([
+             'work_hours' => '2',
+             'company_id' => '1',
+             'liqueur_parameter_id' => '1',
+             'representation_id' => '1',
+             'license_id' => '1'
+
+         ]);
 
     }
 }
