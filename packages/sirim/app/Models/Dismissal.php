b65c13db2e8871951d72f6e9436fd5499b5f1339
Jesus Ordosgoitty <jesuodz@gmail.com>
2022-04-21 23:45:26 -0400

    Bring migration file to backend

--- packages/sirim/app/Models/Dismissal.php
+++ packages/sirim/app/Models/Dismissal.php
@@ -2,13 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Dismissal extends Model
 {
-    use HasFactory;
-
     protected $table = 'dismissals';
 
     protected $fillable = [
@@ -33,5 +30,4 @@ class Dismissal extends Model
     {
         return $this->belongsTo(Taxpayer::class);
     }
-
 }
