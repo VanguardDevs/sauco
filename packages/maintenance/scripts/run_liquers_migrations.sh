fae8274e818a7cefeed59aa3f5d9e28e1a5b5a5c
Jesus Ordosgoitty <jesuodz@gmail.com>
2022-07-12 18:47:48 -0400

    Create liquers migrations script

---
+++ packages/maintenance/scripts/run_liquers_migrations.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+php artisan migrate --path=database/migrations/2022_03_14_105733_create_liqueur_zones_table.php
+php artisan migrate --path=database/migrations/2022_03_14_105848_create_liqueur_classifications_table.php
+php artisan migrate --path=database/migrations/2022_03_14_110218_create_liqueur_parameters_table.php
+php artisan migrate --path=database/migrations/2022_03_14_111011_create_liqueurs_table.php
+php artisan migrate --path=database/migrations/2022_03_16_101401_create_annexed_liqueurs_table.php
+php artisan migrate --path=database/migrations/2022_03_16_101744_create_liqueur_annexes_table.php
+php artisan migrate --path=database/migrations/2022_06_14_105831_create_liqueur_liquidation_table.php
+php artisan migrate --path=database/migrations/2022_06_16_202120_create_requirements_table.php
