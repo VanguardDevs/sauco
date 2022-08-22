67128d74839354f0b3df1de9194daba924727b80
Luisandra29 <luisandrasifontes@gmail.com>
2022-08-22 19:07:52 -0400

    Update Dismissal

--- packages/sirim/resources/views/pdf/reports/dismissals.blade.php
+++ packages/sirim/resources/views/pdf/reports/dismissals.blade.php
@@ -5,7 +5,7 @@
         <style>
             body {
                 font-family: 'Helvetica';
-                font-size: 15px;
+                font-size: 12px;
             }
             .header {
                 width: 100%;
@@ -19,33 +19,6 @@
             #mayorLOGO {
                 float: right;
                 margin-top: -10px;
-            }
-            table, td, th {
-                border: 1px #000 solid;
-            }
-            td {
-                font-size: 12px;
-                padding: 2px 1px;
-            }
-            table {
-                border-collapse: collapse;
-                width: 100%;
-                margin-top: 5px;
-            }
-            .details td {
-                text-align: center;
-            }
-            .titulo{
-                display: block;
-                align: center;
-                text-align: center;
-            }
-
-            #fecha-pdf{
-                display: block;
-                align: right;
-                text-align: right;
-
             }
             .description {
                 text-align: center;
@@ -64,7 +37,8 @@
             }
             .row {
                 display: block;
-                padding-left: 10px;
+                padding-left: 8px;
+                padding-right: 8px;
             }
             .text-center {
                 text-align: center;
@@ -75,20 +49,31 @@
                 /* z-index: 1000; */
                 text-transform: uppercase;
                 font-weight: 700;
-                font-size: 12px;
+                font-size: 11px;
                 margin: auto;
-                margin-top: 10%;
+                margin-top: 4%;
                 /* position: absolute; */
                 left: 10%;
             }
             #watermark {
                 position: absolute;
-                top: 175px;
-                right: 150px;
-                width: 400px;
-                height: 400px;
+                top: 120px;
+                right: 210px;
+                width: 250px;
+                height: 250px;
                 opacity: 0.3;
                 z-index: -1;
+            }
+            .tables {
+                display:block;
+                border-color: darkgreen;
+                border-width: 1.5px;
+                border-style: solid;
+                margin-top: -6px;
+            }
+            h4{
+                text-align: center;
+                margin-top: 1px;
             }
         </style>
     </head>
@@ -115,36 +100,38 @@
                     <img src="{{ asset('/assets/images/logo_alcaldia.jpg') }}" height="79px" width="130px" alt="logo" />
                 </div>
             </div>
-            <br>
-            <div class="row">
-                <p class="titulo">RESOLUCIÓN DE CESE DE ACTIVIDADES</p>
-            </div>
-            <br>
+            <div class="tables">
+                
+                    <h4>RESOLUCIÓN DE CESE DE ACTIVIDADES</h4>
+                
+                <div class="row" style="text-align: justify;">
+                El suscrito, VENANCIO MEZA DÍAZ, actuando en este acto en su carácter de Superintendente Municipal Tributario adscrito a la función ejecutiva de la municipalidad de Bermúdez del estado Sucre, visto la solicitud presentada por el ciudadano(a) {{ $license->representation->person->name }}, titular de la cédula de identidad {{ $license->representation->person->document }}, en su carácter de Presidente de la Sociedad Mercantil {{ $license->taxpayer->commercialDenomination->name }}, identificada con el RIF {{ $license->taxpayer->rif }}, Poseedor de la Licencia de Ejercicio de Actividades Económicas, Registro N.º {{ $license->num }}, y cumplido los requisitos establecidos.
+                </div>
+                
+                <p style="text-align:center;"><u>RESUELVE</u></p>
+                <div class="row" style="text-align: justify;">
+                    <u>Primero:</u> Se otorga  Cese de Ejercicio de Actividades Económicas,  al contribuyente {{ $license->taxpayer->commercialDenomination->name }}, RIF {{ $license->taxpayer->rif }}, Registro Municipal N.º {{ $license->num }}, por tanto, desde la presente fecha se desincorpora la misma de los registros que al efecto mantiene esta dependencia de Administración Tributaria - -- - -
+                </div>
+                <br>
+                <div class="row" style="text-align: justify;">
+                    <u>Segundo:</u> Notifiquese del presente acto al ciudadano(a) {{ $license->representation->person->name }}, C.I {{ $license->representation->person->document }} en su carácter de representante legal de la Sociedad Mercantil {{ $license->taxpayer->commercialDenomination->name }}.
+                </div>
+                <br>
+                <div class="row">
+                    <strong>Carúpano</strong>, {{ date('d-m-Y') }}
+                </div>
 
-            <div class="row" style="text-align: justify;">
-            El suscrito, VENANCIO MEZA DÍAZ, actuando en este Acto en su carácter de Superintendente Municipal Tributario  ------ a la función ejecutiva de la municipalidad de Bermúdez del estado Sucre, visto la solicitud presentada por el ciudadano(a) {{ $license->representation->person->name }}, titular de la cédula de identidad {{ $license->representation->person->document }}, en su carácter de Presidente de la Sociedad Mercantil {{ $license->taxpayer->commercialDenomination->name }}, identificada con el RIF {{ $license->taxpayer->rif }}, Poseedor de la Licencia de Ejercicio de Actividades Económicas, Registro N.º {{ $license->num }}, y cumplido los requisitos establecidos
+                <div class="row">
+                    <strong>El Suscrito</strong>
+                </div>
+                <div class="bottom text-center">
+                    <span class="row">{{ $signature->title }}</span>
+                    <span class="row">superintendente de administración tributaria</span>
+                    <span class="row">{{ $signature->decree }}</span>
+                    <span class="row">GACETA MUNICIPAL EXTRAORDINARIA Nº 378 DE FECHA 30-11-2021</span>
+                </div>
             </div>
-            <br><br>
-            <p style="text-align:center;"><u>RESUELVE</u></p>
-            <div class="row" style="text-align: justify;">
-                <u>Primero:</u> Se otorga  Cese de Ejercicio de Actividades Económicas,  al contribuyente {{ $license->taxpayer->commercialDenomination->name }}, RIF {{ $license->taxpayer->rif }}, Registro Municipal N.º {{ $license->num }}, por tanto, desde la presente fecha se desincorpora la misma de los registros que al efecto mantiene esta dependencia de Administración Tributaria - -- - -
-            </div>
-            <br>
-            <div class="row" style="text-align: justify;">
-                <u>Segundo:</u> Notifiquese del presente acto al ciudadano(a) {{ $license->representation->person->name }}, C.I {{ $license->representation->person->document }} en su carácter de representante legal de la Sociedad Mercantil {{ $license->taxpayer->commercialDenomination->name }}.
-            </div>
-            <br>
-            <div class="row">
-                <strong>Carúpano</strong>, {{ date('d-m-Y') }}
-            </div>
-
-
-            <div class="bottom text-center">
-                <span class="row">{{ $signature->title }}</span>
-                <span class="row">superintendente de administración tributaria</span>
-                <span class="row">{{ $signature->decree }}</span>
-                <span class="row">GACETA MUNICIPAL EXTRAORDINARIA Nº 378 DE FECHA 30-11-2021</span>
-            </div>
+            
         </div>
     </body>
 </html>
