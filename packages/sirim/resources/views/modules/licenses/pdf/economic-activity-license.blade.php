5bec880d975eca49d1c39c1e3c9eb69c34cbd143
Luisandra29 <luisandrasifontes@gmail.com>
2022-10-09 19:43:58 -0400

    Update Economic Activity License Header

--- packages/sirim/resources/views/modules/licenses/pdf/economic-activity-license.blade.php
+++ packages/sirim/resources/views/modules/licenses/pdf/economic-activity-license.blade.php
@@ -140,9 +140,9 @@
         <div class="container">
             <div id="degrade">
                 @if($license->correlative->correlative_type_id == 1)
-                <img src="{{ asset('/assets/images/degrade-4.jpeg') }}" height="85px" width="100%" alt="sumatlogo"/>
-                @else
                 <img src="{{ asset('/assets/images/degrade-3.jpeg') }}" height="85px" width="100%" alt="sumatlogo"/>
+                @else
+                <img src="{{ asset('/assets/images/degrade-4.jpeg') }}" height="85px" width="100%" alt="sumatlogo"/>
                 @endif
             </div>
             <div class="header">
