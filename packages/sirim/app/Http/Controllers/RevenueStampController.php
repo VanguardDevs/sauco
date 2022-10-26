70c948850a84e15e7b706a379a68c4ee1c887d72
Luisandra29 <luisandrasifontes@gmail.com>
2022-10-26 12:22:28 -0400

    Update Listing Licenses

--- packages/sirim/app/Http/Controllers/RevenueStampController.php
+++ packages/sirim/app/Http/Controllers/RevenueStampController.php
@@ -33,11 +33,27 @@ class RevenueStampController extends Controller
             return DataTables::eloquent($query)->toJson();
         }
 
+
+        $stamps = RevenueStamp::query()
+        ->join('licenses', 'licenses.id', '=', 'revenue_stamps.license_id')
+        ->join('taxpayers', 'taxpayers.id', '=', 'licenses.taxpayer_id')
+        ->where('taxpayers.id', $taxpayer->id)
+        ->where('licenses.active', 'true')
+        ->select('revenue_stamps.license_id')
+        ->get();
+
         $existingLicenses = DB::table('licenses')
             ->where('taxpayer_id', $taxpayer->id)
             ->where('ordinance_id', 6)
             ->where('expiration_date', '>', Carbon::now())
             ->where('active', 'true')
+
+            ->where(function($query) use ($stamps) {
+                foreach($stamps as $stamp){
+                    $query->where('id', '<>', $stamp->license_id);
+                }
+            })
+            
             ->groupBy('num', 'id')
             ->having('num', '>', 1)
             ->pluck('num', 'id')
