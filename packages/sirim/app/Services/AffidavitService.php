decb612a7375044987d3cb47d5ef01a3824a3bea
Luisandra29 <luisandrasifontes@gmail.com>
2023-11-16 12:59:20 -0400

    Update affidavits calculation

--- packages/sirim/app/Services/AffidavitService.php
+++ packages/sirim/app/Services/AffidavitService.php
@@ -19,13 +19,33 @@ class AffidavitService
         $bruteAmounts = $amounts;
         $totalAmounts = Array();
 
+        /*$firstAffidavit=$affidavits[0]->economicActivity;
+        $secondAffidavit=$affidavits[1];*/
+
+        $firstAffidavit = $affidavits->first();
+        $secondAffidavit = $affidavits->get(1);
+
+        /*
+        $secondAffidavit = $affidavits->skip(1)->first();
+        $secondAffidavit = $affidavits->skip(1)->take(1)->first();
+         */
+
+        
         foreach($affidavits as $affidavit) {
             $amount = array_shift($bruteAmounts);
 
             if (($affidavits->count() > 2) && ($amount == 0.00)) {
-                $updateSettlement = $this->calculateTax($month, $affidavit, $amount);
-            } else {
-                $updateSettlement = $this->calculateTax($month, $affidavit, $amount, true);
+                $updateSettlement = $this->calculateTax($month, $affidavit, $amount, false, $affidavits);
+            }
+           /* elseif($affidavits->count() == 2){
+            
+                if ($firstAffidavit->economicActivity->min_tax > $secondAffidavit->economicActivity->min_tax) {
+                    //$maxDeclaration = $affidavit;
+                    $updateSettlement = $this->calculateTax($month, $affidavit, $amount, true, $affidavits);
+                }
+            }*/
+            else {
+                $updateSettlement = $this->calculateTax($month, $affidavit, $amount, true, $affidavits);
             }
 
             array_push($totalAmounts, $updateSettlement->amount);
@@ -54,10 +74,10 @@ class AffidavitService
             }
         }
 
-        return $this->calculateTax($month, $maxDeclaration, $amount, true)->amount;
+        return $this->calculateTax($month, $maxDeclaration, $amount, true, $affidavits)->amount;
     }
 
-    public function calculateTax(Month $month, EconomicActivityAffidavit $affidavit, $amount, $update = false)
+    public function calculateTax(Month $month, EconomicActivityAffidavit $affidavit, $amount, $update = false, $arrayAffidavits)
     {
 
         $currentYear = Carbon::now()->year;
@@ -95,6 +115,37 @@ class AffidavitService
                 if ($total < $minTax || $amount == 0.00) {
                     $total = $minTax;
                 }
+
+                    /////////////////////
+                if($arrayAffidavits->count() == 2){
+                    $firstAffidavit = $affidavits->first();
+                    $secondAffidavit = $affidavits->get(1);
+
+                    if($firstAffidavit->economicActivity->min_tax = $secondAffidavit->economicActivity->min_tax){
+
+                    }
+
+                }
+////////////////////////////////////////////////////////
+
+                /*Cuando las 2 actividades poseen min_tax y alicuota iguales, y ambas califican para minimo tributable se aplica el 
+                MINIMO a la actividad de MENOR INGRESO y la otra actividad por ALICUOTA*/
+
+                /*Cuando el min_tax es diferente pero la Alicuota es igual para las 2 actividades, y ambas Califican para MINIMO
+                se aplica el MINIMO a la actividad con MAYOR min_tax y a la otra actividad se le cobra la alicuota*/
+
+                /*Cuando tanto el min_tax como la alicuota de ambas actividades es diferente y ambas califican para 
+                MINIMO TRIBUTABLE se aplica el MINIMO a la actividad con Mayor min_tax y a la otra actividad se le cobra la alicuota*/
+
+                /// Mas de 2 actividades ///
+
+                /*Cuando las actividades poseen min_tax y alicuota iguales, y todas califican para minimo tributable se aplica el 
+                MINIMO a la actividad de MENOR INGRESO y las otras actividades por ALICUOTA*/
+
+                /*Cuando el min_tax es diferente en al menos una actividad pero la Alicuota es igual para todas 
+                las actividades, se liquida por MINIMO la actividad con MAYOR min_tax y la actividad que DECLARA MENOS
+                el resto se cobra por alicuota */
+
             }
         }
 
